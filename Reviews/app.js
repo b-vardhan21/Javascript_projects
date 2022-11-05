77c6949b7c16df4eeb2613eb94e24813a49be712
vardhan barishetti <67987365+b-vardhan21@users.noreply.github.com>
2022-11-05 00:20:10 -0400

    Review completed

--- Reviews/app.js
+++ Reviews/app.js
@@ -43,5 +43,52 @@ const author = document.getElementById('author');
 const job = document.getElementById('job');
 const info = document.getElementById('info');
 
-const prevBtn = 
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
+// set starting item
+
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded',function(){
+    showPerson(currentItem);
+
+})
+
+// show person based on item
+
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener('click',function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+// prev person
+
+prevBtn.addEventListener('click',function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+randomBtn.addEventListener('click',function(){
+    currentItem = Math.floor(Math.random()*4);
+    showPerson(currentItem);
+
+});
